1383a9b9a00d79c6210cf8d7bc729ec72db07b88
EzexnDev <gianmarco.cavalieredev@gmail.com>
2020-11-17 16:57:43 +0100

    Esercizio completato con selettore

--- js/data.js
+++ js/data.js
@@ -1,13 +1,13 @@
 const data = [{
     "id": "0",
-    "src": "../img/image1.jpg",
+    "src": "/img/image1.jpg",
 }, {
     "id": "1",
-    "src": "../img/image2.jpg",
+    "src": "/img/image2.jpg",
 }, {
     "id": "2",
-    "src": "../img/image3.jpg",
+    "src": "/img/image3.jpg",
 }, {
     "id": "3",
-    "src": "../img/image4.jpg",
+    "src": "/img/image4.jpg",
 }];

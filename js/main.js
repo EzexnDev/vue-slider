c0ba6de45cccee4cfa5ed4ad945260719ba38281
EzexnDev <gianmarco.cavalieredev@gmail.com>
2020-11-17 16:40:04 +0100

    Fine

--- js/main.js
+++ js/main.js
@@ -6,9 +6,12 @@ const sliderApp = new Vue({
     },
     methods: {
         movePrev: function() {
+            // IF -> True Torno all'inizio
+            // IF -> False Torno indietro di 1
             (this.currentSrcIndex == 0) ? this.currentSrcIndex = this.imgSources.length - 1: this.currentSrcIndex--;
         },
-
+        // IF -> True Torno all'inizio
+        // IF -> False vado avanti di 1
         moveNext: function() {
             (this.currentSrcIndex == this.imgSources.length - 1) ? this.currentSrcIndex = 0: this.currentSrcIndex++;
         }
